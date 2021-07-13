927113a8dfe7602930e40523c5492a18c73212e2
Yago Peixinho <yagopeixinho03@gmail.com>
2021-07-13 13:58:15 -0300

    v7.3

--- questionsHall/ciencias_humanas/historia/questionsHistory.js
+++ questionsHall/ciencias_humanas/historia/questionsHistory.js
@@ -204,8 +204,137 @@ const questions = [
       { text: 'Reformulação do acesso ao ensino superior e do financiamento científico nacional.', correct: false },
       { text: 'Reforma das políticas macroeconômicas e dos mecanismos de controle inflacionário.', correct: true },
     ]
-  }
+  },
 
+  {
+    question: '(ENEM - 2020) A Divisão Internacional do Trabalho significa que alguns países se especializam em ganhar e outros, em perder. Nossa comarca no mundo, que hoje chamamos de América Latina, foi precoce: especializou-se em perder desde os remotos tempos em que os europeus do Renascimento se aventuraram pelos mares e lhe cravaram os dentes na garganta. Passaram-se os séculos e a América Latina aprimorou suas funções. Escrito na década de 1970, o texto considera a participação da América Latina na Divisão Internacional do Trabalho marcada pela...',
+    
+    resolution: 'O imperialismo, colonial, aproveitou de nossos recursos territoriais para implantar esse sistema agroexportador e extrativista na América Latina, que se desenvolveu com o passar do tempo, mas, não se alterou.',
+    answers: [
+      { text: 'produção inovadora de padrões de tecnologia.', correct: false },
+      { text: 'superação paulatina do caráter agroexportador.', correct: false },
+      { text: 'apropriação imperialista dos recursos territoriais.', correct: true },
+      { text: 'valorização econômica dos saberes tradicionais', correct: false },
+      { text: 'dependência externa do suprimento de alimentos.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) A reabilitação da biografia histórica integrou as aquisições da história social e cultura, oferecendo aos diferentes atores históricos uma importância diferenciada, distinta, individual. Mas não se tratava mais de fazer, simplesmente, a história dos grandes nomes, em formato hagiográfico — quase uma vida de santo —, sem problemas, nem máculas. Mas de como examinar os atores (ou o ator) célebres ou não, como testemunhas, como reflexos, como reveladores de uma época. De acordo com o texto, novos estudos têm valorizado a história do indivíduo por se constituir como possibilidade de...',
+    
+    resolution: 'A historiografia ao ampliar as suas fontes e valorizar a história de múltiplos sujeitos incluindo indivíduos comuns vê por meio destes a possibilidade de acesso ao cotidiano das comunidades. “como examinar os atores (ou o ator) célebres ou não, como testemunhas, como reflexos, como reveladores de uma época.”',
+    answers: [
+      { text: 'adesão ao método positivista.', correct: false },
+      { text: 'expressão do papel das elites.', correct: false },
+      { text: 'resgate das narrativas heroicas.', correct: false },
+      { text: 'acesso ao cotidiano das comunidades.', correct: true },
+      { text: 'interpretação das manifestações do divino.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Na Grécia, o conceito de povo abrange tão somente aqueles indivíduos considerados cidadãos. Assim é possível perceber que o conceito do povo era muito restritivo. Mesmo tendo isso em conta, a forma democrática vivenciada e experimentada pelos gregos atenienses nos séculos IV e V a.C. pode ser caracterizada, fundamentalmente, como direta. Naquele contexto, a emergência do sistema de governo mencionado no excerto promoveu o(a):',
+    
+    resolution: 'Com a democracia direta, os indivíduos agraciados com o status de cidadania poderiam participar diretamente das decisões políticas. ',
+    answers: [
+      { text: 'competição para escolha de representantes.', correct: false },
+      { text: 'campanha pela revitalização das oligarquias.', correct: false },
+      { text: 'estabelecimento de mandatos temporários.', correct: false },
+      { text: 'declínio da sociedade civil organizada.', correct: false },
+      { text: 'participação do exercício do poder.', correct: true },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Com efeito, até a destruição de Cartago, o povo e o Senado romano governavam a República em harmonia e sem paixão, e não havia entre os cidadãos luta por glória ou dominação, o medo do inimigo mantinha a cidade no cumprimento do dever. Mas, assim que o medo desapareceu dos espíritos, introduziram-se os males pelos quais a prosperidade tem predileção, isto é, a libertinagem e o orgulho. O acontecimento histórico mencionado no texto de Salústio, datado de I a.C., manteve correspondência com o processo de...',
+    
+    resolution: 'A destruição de Cartago por Roma foi definitiva para o processo de expansão desta sociedade. A partir desta conquista a então República que posteriormente transformou-se em um Império passou a possuir como um característica primordial o expansionismo.',
+    answers: [
+      { text: 'demarcarção de terras públicas.', correct: false },
+      { text: 'imposição da escravidão por dívidas..', correct: false },
+      { text: 'restrição da cidadania por parentesco.', correct: false },
+      { text: 'restauração das instituições ancestrais.', correct: false },
+      { text: 'expansão das fronteiras extra peninsulares.', correct: true },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Com os ideais liberais oriundos do pensamento iluminista, trazidos à América especialmente pelos emigrantes que foram se estabelecer na Nova Inglaterra, pode-se observar o desenvolvimento de uma situação social de regulamentação da propriedade e de liberdade comercial, o que ilustra, especialmente, a consolidação dos valores burgueses implementados nesses ideais.',
+    
+    resolution: 'Na década de 90 um dos grandes problemas do Brasil era sem dúvidas a inflação como uma consequência da década de 80. Os governos focaram em tentar controlar a inflação e em reformas políticas macroeconômica.',
+    answers: [
+      { text: 'emprego do trabalho escravo.', correct: false },
+      { text: 'consolidação dos valores burgueses.', correct: true },
+      { text: 'banimento das dissidências religiosas.', correct: false },
+      { text: 'contenção da identificação nacionalista', correct: false },
+      { text: 'hierarquização dos agentes econômicos', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) O toyotismo, a partir dos anos 1970, teve grande impacto no mundo ocidental, quando se mostrou para os países avançados como uma opção possível para a superação de uma crise de acumulação. A característica organizacional do modelo em questão, requerida no contexto de crise, foi o(a)',
+    
+    resolution: 'O toyotismo veio, justamente, para efetuar uma produção que se adequava à demanda, para que não ocorresse a mesma crise do ano de 1929.',
+    answers: [
+      { text: 'expansão dos grandes estoques.', correct: false },
+      { text: 'incremento da fabricação em massa.', correct: false },
+      { text: 'adequação da produção à demanda.', correct: true },
+      { text: 'aumento da mecanização do trabalho.', correct: false },
+      { text: 'centralização das etapas de planejamento.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Porque todos confessamos não se pode viver sem alguns escravos, que busquem a lenha e a água, e façam cada dia o pão que se come, e outros serviços que não são possíveis poderem-se fazer pelos Irmãos Jesuítas, máxime sendo tão poucos, que seria necessário deixar as confissões e tudo mais. Parece-me que a Companhia de Jesus deve ter e adquirir escravos, justamente, por meios que as Constituições permitem, quando puder para nossos colégios e casas de meninos. O texto explicita premissas da expansão ultramarina portuguesa ao buscar justificar a...',
+    
+    resolution: 'A partir do texto de apoio da questão, nota-se o esforço por parte da Companhia de Jesus em legitimar e promover o uso da mão de obra escravizada em suas funções desempenhadas na colônia, algo que explicita as premissas da expansão ultramarina portuguesa na medida em que busca legitimar o cativeiro em prol dos movimentos de aculturação e catequização desempenhados por eles.',
+    answers: [
+      { text: 'propagação do ideário cristão.', correct: false },
+      { text: 'valorização do trabalho braçal.', correct: false },
+      { text: 'adoção do cativeiro na Colônia.', correct: true },
+      { text: 'adesão ao ascetismo contemplativo.', correct: false },
+      { text: 'alfabetização dos indígenas nas Missões.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Os representantes do povo francês, tendo em vista que a ignorância, o esquecimento ou o desprezo dos direitos do homem são as únicas causas das infelicidades públicas e da corrupção dos governos, resolveram declarar solenemente os direitos naturais, inalienáveis e sagrados do homem, a fim de que esta declaração, sempre presente em todos os membros do corpo social, lhes lembre permanentemente seus direitos e seus deveres; a fim de que as reivindicações dos cidadãos, fundadas em princípios simples e incontestáveis, se dirijam sempre à conservação da Constituição e à felicidade geral. Esse documento, elaborado no contexto da Revolução Francesa, reflete uma profunda mudança social ao estabelecer a...',
+    
+    resolution: 'A partir desse documento, considerava-se todos os homens como iguais na França, desde que dotados do status de cidadania.',
+    answers: [
+      { text: 'manutenção das terras comunais.', correct: false },
+      { text: 'supressão do poder constituínte.', correct: false },
+      { text: 'falência da sociedade burguesa.', correct: false },
+      { text: 'paridade no tratamento jurídico.', correct: true },
+      { text: 'abolição dos partidos políticos.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) Ao abrigo do teto, sua jornada de fé começava na sala de jantar.  Na équena célula cristã, dividia-se a refeição e durante elas os crentes conversavam, rezavam e liam cartas de correligionários residentes em locais diferentes do Império Romano (século II da Era Cristã). Esse ambiente garantia peculiar apoio emocional às experiências intensamente individuais que abrigava. Um motivo que explica a ambientação da prática descrita no texto encontra-se no(a)',
+    
+    resolution: 'Nesse contexto, somente poder-se-ia professar a fé oficial do Império.',
+    answers: [
+      { text: 'regra judaica, que pregava a superioridade espiritual dos cultos das sinagogas.', correct: false },
+      { text: 'moralismo da legislação, que dificultava as reuniões abertas da juventude livre.', correct: false },
+      { text: 'adesão ao patriciado, que subvertia o conceito original dos valores estrangeiros.', correct: false },
+      { text: 'decisão política, que censurava as manifestações públicas da doutrina dissidente.', correct: true },
+      { text: 'violência senhorial, que impunha a desestruturação forçada das famílias escravas.', correct: false },
+    ]
+  },
+
+  {
+    question: '(ENEM - 2020) É difícil imaginar que nos anos 1990, num país com setores da população na pobreza absoluta e sem uma rede de benefícios sociais em que se apoiar, um governo possa abandonar o papel de promotor de programas de geração de emprego, de assistência social, de desenvolvimento da infraestrutura e de promoção de regiões excluídas, na expectativa de que o mercado venha algum dia a dar uma resposta adequada a tudo isso. Nesse contexto, a criticada postura dos governos frente à situação social do país coincidiu com a priorização de que medidas?',
+    
+    resolution: 'Na década de 90 um dos grandes problemas do Brasil era sem dúvidas a inflação como uma consequência da década de 80. Os governos focaram em tentar controlar a inflação e em reformas políticas macroeconômica.',
+    answers: [
+      { text: 'Expansão dos investimentos nas empresas públicas e nos bancos estatais.', correct: false },
+      { text: 'Democratização do crédito habitacional e da aquisição de moradias populares..', correct: false },
+      { text: 'Enxugamento da carga fiscal individual e da contribuição tributária empresarial.', correct: false },
+      { text: 'Reformulação do acesso ao ensino superior e do financiamento científico nacional.', correct: false },
+      { text: 'Reforma das políticas macroeconômicas e dos mecanismos de controle inflacionário.', correct: true },
+    ]
+  },
 
 
 
